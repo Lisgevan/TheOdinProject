6057124e6d2f9d9a7bb23872144583543690507d
Lisgevan <episimo@hotmail.com>
2022-02-01 23:30:07 +0200

    form project completed

---
+++ 02_Full_Stack_JavaScript/01_INTERMEDIATE_HTML_AND_CSS/03_Project_Sign_up_form/scripts.js
@@ -0,0 +1,57 @@
+// element selectors
+let form = document.querySelector("form");
+let button = document.querySelector("button");
+
+const toggleMessage = e => {
+	const targetEl = document.querySelector(`#${e.target.name} ~ span`);
+	trueOrFalse(e) ? targetEl.classList.remove("hide") : targetEl.classList.add("hide");
+};
+
+const trueOrFalse = e => {
+	switch (e.target.name) {
+		case "firstName":
+		case "lastName":
+			return e.target.value.length < 1;
+		case "email":
+			const regexMail =
+				/^(?:(?:[\w`~!#$%^&*\-=+;:{}'|,?\/]+(?:(?:\.(?:"(?:\\?[\w`~!#$%^&*\-=+;:{}'|,?\/\.()<>\[\] @]|\\"|\\\\)*"|[\w`~!#$%^&*\-=+;:{}'|,?\/]+))*\.[\w`~!#$%^&*\-=+;:{}'|,?\/]+)?)|(?:"(?:\\?[\w`~!#$%^&*\-=+;:{}'|,?\/\.()<>\[\] @]|\\"|\\\\)+"))@(?:[a-zA-Z\d\-]+(?:\.[a-zA-Z\d\-]+)*|\[\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\])$/gi;
+			return !regexMail.test(e.target.value);
+		case "tel":
+			const regexTel =
+				/(^[0-9]\d{3}[ ]\d{3}[ ]\d{3}$)|(^[+][0-9]\d{1}[ ]\d{1}[ ]\d{4}[ ]\d{4}$)|(^[+][0-9]\d{1}[ ]\d{3}[ ]\d{3}[ ]\d{3}$)|(^[0-9]\d{3}[ ]\d{3}[ ]\d{3}$)|(^[0-9]\d{9}$)|(^[0-9]\d{14}$)|(^([(]([0-9]\d{1})[)][ ])([0-9]\d{3})[ ]\d{4}$)/gm;
+			return !regexTel.test(e.target.value);
+		case "password":
+			const regexPas = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm;
+			return !regexPas.test(e.target.value);
+		case "confirmPassword":
+			let password = document.querySelector("#password").value;
+			return password !== e.target.value;
+		default:
+			break;
+	}
+};
+
+form.addEventListener("click", e => {
+	switch (e.target.localName) {
+		case "input":
+			toggleMessage(e);
+			break;
+		default:
+			break;
+	}
+});
+
+form.addEventListener("input", e => {
+	switch (e.target.localName) {
+		case "input":
+			toggleMessage(e);
+			break;
+		default:
+			break;
+	}
+});
+
+button.addEventListener("click", e => {
+	e.preventDefault;
+	form.checkValidity() ? alert("Account created") : alert("Please correct invalid inputs!");
+});
